327be3f44984d6ca845f195bc905c9d61b626095
syanjaliRamu <mailtoramu38@gmail.com>
2021-06-30 07:54:34 +0545

    python practice

---
+++ introduction.py
@@ -0,0 +1,40 @@
+# print("hello python")
+# print("2008")
+# print("ram " "hari" )
+# print('mothre'\s')
+
+# # x = 10
+# print(x)
+# fName = "Ram Prasad"
+# lName = "Neupane"
+# age = 30
+# print("Name :{} {} AGe: {}".format(fName, lName, age))
+# print(f"Name : {fName} Age:{age}")
+# a=20
+# b=10
+# c=a
+# print(a)
+# print(b)
+# print(c)
+# fName = "Hari"
+# mName = "sharan"
+# lName = "Neupane"
+# age = 39
+# home = "Syangja"
+#
+# print(f"Name:{fName} {mName} {lName} Age:{age} Home:{home}")
+# x=120
+# x = 6664j
+# print(type(x))
+# print(dir(x))
+# x=input("input any number:")
+# x = input("enter x:")
+# y = input("enter y:")
+# # x = int(x)
+# # y = int(y)
+# # print(x+y)
+# total=int(x)+int(y)
+# print(total)
+fName = "Aarya"
+lName = "Neupane"
+print(fName+' '+ lName)
