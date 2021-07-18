eb5c09c6620c0342dc263575094279be15827cf3
syanjaliRamu <mailtoramu38@gmail.com>
2021-07-18 08:18:01 +0545

    cal

---
+++ next_ex.py
@@ -0,0 +1,9 @@
+def my_repeat(data ,time):
+    increment = 1
+    values = []
+    while increment <= times:
+        values.append(data)
+        increment  += 1
+    retun (values)
+
+print (my_repeat( 'ram,3'))
